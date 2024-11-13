4b6d417a39e4ecece6fa8ef29f235feff1c2ff0f
Vyacheslav <slawa.121@yandex.ru>
2024-11-13 10:56:14 +0300

    убираем Data

--- ElecticStore-admin/src/main/java/labs/electicstoreadmin/Category.java
+++ ElecticStore-admin/src/main/java/labs/electicstoreadmin/Category.java
@@ -4,21 +4,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 @AllArgsConstructor
-@NoArgsConstructor()
+@NoArgsConstructor
 @Entity
 public class Category {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @EqualsAndHashCode.Include
   private int id;
+
   private String name;
 
-  // Конструктор для создания категории с именем
   public Category(String name) {
     this.name = name;
   }
 }
-
